8de3176703ccffc2547a8d43ce21271053516e19
pvaldivia <pvald016@ucr.edu>
2021-01-08 19:01:56 -0800

    Added Project desc & bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import reportWebVitals from './reportWebVitals';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+        <Card>
+          <Card.Header> Contractor, Malwarebytes Inc </Card.Header>
+          <Card.Text>
+            <ul>
+              <li>Create automated ETL Pipelines using Workato and Snowflake</li>
+              <li>ETL pipelines ingest data that is used for analysis to gain insights on how to improve business</li>
+            </ul>
+          </Card.Text>
+        </Card>
+      </Container>
   );
 }
 
