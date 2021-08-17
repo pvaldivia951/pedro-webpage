33f7dbc7a86061b50423b0d8b8e4ea27688a22c3
pvaldivia <pvaldivia@malwarebytes.com>
2021-08-17 01:47:32 -0700

    removed Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import './App.css';
 import profileImage from './pedros_profile_image.png';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 
 class Overlay extends React.Component {
   render () {
     return (
-      <div>
-      <Navbar className="Prmy-Section-Color" variant="dark">
-          <Navbar.Brand href="#home">Pedro's Resume</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#education">Education</Nav.Link>
-            <Nav.Link href="#exp">Experience</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#hobbies">Hobbies</Nav.Link>
-          </Nav>
-      </Navbar>
-      <br></br>
-      <br></br>
+      <div className="top-nav">
+        <a href="#about"> About </a>
+        <a href="#experience"> Experience </a>
+        <a href="#skills"> Skills </a>
+        <a href="#education"> Education </a>
+        <a href="#hobbies"> Hobbies </a>
       </div>
+      
     );
   }
 }
@@ -31,22 +21,18 @@ class Overlay extends React.Component {
 
 function Experience(props) {
   return (
-  <Container>
-    <Row className="justify-content-center">
-      <Col lg={3}>
-        <div className="Prmy-Text-Color">
-          <h6>{props.company}</h6>
-          <p>{props.period}</p> 
-        </div>
-      </Col>
-      <Col lg={5}>
-        <div className="Prmy-Text-Color">
-          <h4>{props.job_title}</h4>
-          <p>{props.description}</p>
-        </div>
-      </Col>
-    </Row>
-    </Container>
+  <>
+  <div className="my-container">
+    <div className="Prmy-Text-Color small">
+      <h6>{props.company}</h6>
+      <p>{props.period}</p> 
+    </div>
+    <div className="Prmy-Text-Color">
+      <h4>{props.job_title}</h4>
+      <p>{props.description}</p>
+    </div>
+  </div>
+  </>
   );
 }
 
@@ -58,19 +44,14 @@ function App() {
       {/* About Section */}
       <div>
         <div>
+          <br></br>
           <h1 className="App">Pedro Valdivia</h1>
-        </div>
-        <Container>
-          <hr className="Line-Break-Sndy"/>
           <p className="App">Online Resume</p>
-          <br></br>
-          <br></br>
-          <Row className="justify-content-center" >
-            <Col xs={7} sm={6} md={6} lg="auto">
-              <img className="Border-Color" width={256} height={256} src={profileImage} alt="Profile" />
-            </Col>
-            <Col xs={12}  lg={8}>
-              <h4>About Me</h4>
+        </div>
+        <div className="my-container">
+          <img className="Border-Color" width={256} height={256} src={profileImage} alt="Profile" />
+          <div className="div-padding">
+            <h4>About Me</h4>
               <p>
                 I'm a recent graduate who is working towards growing my developer skills. I'm currently
                 learning new skills involved with web development and data engineering. I created this web
@@ -81,9 +62,8 @@ function App() {
               <p className="p-margin">Age: 24</p>
               <p className="p-margin">Email: pvaldivia951@gmail.com</p>
               <p><a href="https://www.linkedin.com/in/pedro-valdivia1/">LinkedIn</a></p>
-            </Col>
-          </Row>
-        </Container>
+            </div>
+        </div>
         <br></br>
       </div>
 
@@ -91,10 +71,9 @@ function App() {
       <div className="Prmy-Section-Color">
         <br></br>
         <h1 className="App Prmy-Text-Color">Experience</h1>
-        <hr className="Line-Break-Prmy"/>
         <br></br>
         <Experience 
-          job_title="Data Engineer - Contractor"
+          job_title="Data Engineer"
           company="Malwarebytes"
           period="October 2020 - Present"
           description="Create and maintain automated ETL Pipelines using Workato and Snowflake. These pipelines are used to ingest data which is used for analysis to gain insights on how to improve business."
@@ -114,7 +93,6 @@ function App() {
       <div>
       <br></br>
         <h1 className="App Sndt-Text-Color">Skills</h1>
-        <hr className="Line-Break-Sndy"/>
         <br></br>
       </div>
     </div>
